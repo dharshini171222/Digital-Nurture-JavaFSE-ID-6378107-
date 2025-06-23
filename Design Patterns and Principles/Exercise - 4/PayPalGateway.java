e18145c86741a1607575ad7ed5cdb56ec69942e9
dharshini171222 <dharshini171222@gmail.com>
2025-06-23 20:38:48 +0530

    Delete Design Patterns and Principles directory

--- Design Patterns and Principles/Exercise - 4/PayPalGateway.java
+++
@@ -1,5 +0,0 @@
-public class PayPalGateway {
-    public void makePayment(double amountInUSD) {
-        System.out.println("Payment of $" + amountInUSD + " made via PayPal.");
-    }
-}
--- Design Patterns and Principles/Exercise - 4/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void charge(double amountInDollars) {
-        System.out.println("Charged $" + amountInDollars + " through Stripe.");
-    }
-}
