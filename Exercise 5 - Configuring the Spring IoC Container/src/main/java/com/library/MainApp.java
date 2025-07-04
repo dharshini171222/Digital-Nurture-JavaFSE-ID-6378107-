f1fc3df5a5501d1679f8c48315a55081087e0896
dharshini171222 <dharshini171222@gmail.com>
2025-07-04 21:07:02 +0530

    Add files via upload

---
+++ Exercise 5 - Configuring the Spring IoC Container/src/main/java/com/library/MainApp.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load applicationContext.xml
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Call a method to test the configuration
+        bookService.addBook("To Kill a Mockingbird");
+    }
+}
