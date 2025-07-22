fde035e559ce5873ba77fb09ad11ad7cd06e3e23
dharshini171222 <dharshini171222@gmail.com>
2025-07-22 14:06:23 +0530

    Delete week - 4 directory

--- week - 4/exercise 2 - 5/src/main/java/com/example/springrest/exception/CountryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springrest.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Country not found")
-public class CountryNotFoundException extends RuntimeException {
-}
