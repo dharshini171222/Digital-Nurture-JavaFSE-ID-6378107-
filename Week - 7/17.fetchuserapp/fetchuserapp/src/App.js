da09acc7f5732b4ba7ce17aeaf266f84ddb2a423
Dharshini <dharshini171222@gmail.com>
2025-08-03 20:02:00 +0530

    modified

---
+++ Week - 7/17.fetchuserapp/fetchuserapp/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Getuser from './Getuser';
+
+function App() {
+  return (
+    <div className="App">
+      <Getuser />
+    </div>
+  );
+}
+
+export default App;
